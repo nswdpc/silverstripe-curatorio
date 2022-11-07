c57cd385b4245186f9558004cdb87e4a63cb575f
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-11-07 12:47:46 +1100

    (tests) improve and update tests, use a fixture, use the feed record values

--- tests/CuratorWidgetTest.php
+++ tests/CuratorWidgetTest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace  SWDPC\Elemental\Tests\Curator;
+namespace NSWDPC\Elemental\Tests\Curator;
 
+use NSWDPC\Elemental\Models\Curator\CuratorFeed;
 use NSWDPC\Elemental\Models\Curator\ElementCuratorFeedWidget;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Dev\SapphireTest;
@@ -11,38 +12,33 @@ use SilverStripe\View\Requirements;
  * Unit test to verify Curator element handling
  * @author James
  */
-class CuratorFeedWidgetTest extends SapphireTest
+class CuratorWidgetTest extends SapphireTest
 {
 
+    /**
+     * @inheritdoc
+     */
     protected $usesDatabase = true;
 
-    public function setUp() {
-        parent::setUp();
-    }
+    /**
+     * @inheritdoc
+     */
+    protected static $fixture_file = './CuratorWidgetTest.yml';
 
-    public function tearDown()
-    {
-        parent::tearDown();
-    }
-
+    /**
+     * Test the widget template
+     */
     public function testWidget() {
 
-        $record = [
-            'Title' => 'CURATOR_TEST',
-            'CuratorFeedId' => 'abcd123',
-            'CuratorContainerId' => 'container-test-id'
-        ];
+        $element = $this->objFromFixture( ElementCuratorFeedWidget::class, 'testfeedelement1');
+        $feedRecord = $element->CuratorFeedRecord();
 
-        $element = ElementCuratorFeedWidget::create($record);
-        $element->write();
-
-        $this->assertEquals($record['Title'], $element->Title);
-        $this->assertEquals($record['CuratorFeedId'], $element->CuratorFeedId);
-        $this->assertEquals($record['CuratorContainerId'], $element->CuratorContainerId);
+        $this->assertInstanceOf( CuratorFeed::class, $feedRecord );
+        $this->assertTrue( $feedRecord->exists() );
 
         $template = $element->forTemplate();
 
-        $this->assertTrue(strpos($template, "<div id=\"{$record['CuratorContainerId']}\">") !== false, "The div element ID is wrong for the container");
+        $this->assertTrue(strpos($template, "<div id=\"{$feedRecord->CuratorContainerId}\">") !== false, "The div element ID is wrong for the container");
 
         if($element->config()->get('include_powered_by')) {
             $this->assertTrue( strpos($template, "Powered by Curator.io") !== false, "Powered by value should be present" );
@@ -52,7 +48,7 @@ class CuratorFeedWidgetTest extends SapphireTest
 
         $scripts = Requirements::get_custom_scripts();
 
-        $this->assertTrue( is_array($scripts) && array_key_exists("curator_feed_{$record['CuratorFeedId']}", $scripts), "Requirements do not exist" );
+        $this->assertTrue( is_array($scripts) && array_key_exists("curator_feed_{$feedRecord->CuratorFeedId}", $scripts), "Requirements do not exist" );
 
     }
 
